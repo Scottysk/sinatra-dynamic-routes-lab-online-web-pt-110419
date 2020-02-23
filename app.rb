2ea8fcdafc66888cbecbe1d79d68dfa7213d56af
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-02-23 04:40:09 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get '/reversename/:name' do
-    @reverse_name = params[:name].reverse
+    @reversename = params[:name].reverse
     @reverse
   end
   
