c5fed260998a1e64349aaf04dfd72fe08679d3d4
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-02-23 04:41:01 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
   end
   
   get '/square/:number' do
-    @number = params[:number].to_i
+    @number = params[:number].to_i ** 2
   end
   
   get '/say/:number/:phrase' do
